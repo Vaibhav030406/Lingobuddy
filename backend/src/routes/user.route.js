1c82bed00feaad2f4868019764d4b150eefca044
Vaibhav030406 <vaibhev.s.tomar@gmail.com>
2025-09-28 17:41:53 +0530

    error

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -13,4 +13,4 @@ router.post("/friend-request/:id",sendFriendRequest);
 router.put("/friend-request/:id/accept",acceptFriendRequest);
 router.get("/friend-request",getFriendRequests);
 router.get("/outgoing-friend-request",getOutgoingFriendRequests);
-export default router;
+export default routers;
