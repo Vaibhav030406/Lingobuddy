31abd7daf71906d4e15c02eab91d2a05dcfdaf9e
Vaibhav030406 <vaibhev.s.tomar@gmail.com>
2025-10-04 18:24:39 +0530

    same site removed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -83,7 +83,7 @@ export const login = async (req, res) => {
       res.cookie("jwt", token, {
          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
          httpOnly: true,
-         sameSite: "strict",
+         sameSite: "none",
          secure: process.env.NODE_ENV === "production",
       });
 
